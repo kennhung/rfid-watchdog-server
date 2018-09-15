a5ff8abe5053147f80107b2e62edc9d1bea677b7
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-09-16 02:58:04 +0800

    fix write not receive bug

--- src/main/java/indi/kennhuang/rfidwatchdog/server/devices/DeviceHandler.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/devices/DeviceHandler.java
@@ -26,7 +26,8 @@ public class DeviceHandler implements Runnable {
         try {
             input = new DataInputStream(this.clientSocket.getInputStream());
             output = new DataOutputStream(this.clientSocket.getOutputStream());
-
+            auth = true;
+            //TODO remove after auth finish;
             while (true) {
                 //TODO add ping function
                 if (input.available() > 0) {
@@ -38,10 +39,11 @@ public class DeviceHandler implements Runnable {
 
                         HardwareMessage message = new HardwareMessage(inputBuffer.toString());
                         JSONObject reply = new JSONObject();
-                        if(auth || message.type == HardwareMessage.AUTH){
+                        if (auth || message.type == HardwareMessage.AUTH) {
                             switch (message.type) {
                                 case HardwareMessage.CARD_CHECK:
                                     reply = CheckDoorPermission.check(message.content);
+                                    System.out.println(reply.toString());
                                     break;
                                 case HardwareMessage.RESPONSE:
 
@@ -50,13 +52,14 @@ public class DeviceHandler implements Runnable {
 
                                     break;
                                 default:
-
+                                    System.out.println("Wrong type: " + message.type);
                                     break;
                             }
                         }
 
-                        output.writeUTF(reply.toString());
+                        output.write((reply.toString() + ";").getBytes());
                         output.flush();
+                        inputBuffer.delete(0, inputBuffer.length());
                     } else {
                         inputBuffer.append((char) buf);
                     }
