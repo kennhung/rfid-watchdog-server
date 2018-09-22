839f43bf13a0e7727441aa10eff541a200dde5cd
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-09-22 20:42:57 +0800

    add decodeMessage()

--- src/main/java/indi/kennhuang/rfidwatchdog/server/protocal/HardwareMessage.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/protocal/HardwareMessage.java
@@ -1,6 +1,7 @@
 package indi.kennhuang.rfidwatchdog.server.protocal;
 
 import indi.kennhuang.rfidwatchdog.server.protocal.enums.TypesEnum;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class HardwareMessage extends TypesEnum {
@@ -8,17 +9,24 @@ public class HardwareMessage extends TypesEnum {
     public types type;
 
     public HardwareMessage(){
-
+        this.content = new JSONObject();
     }
 
     public HardwareMessage(types type){
+        this.content = new JSONObject();
         this.type = type;
     }
 
-    public static HardwareMessage encodeMessage(String msg){
-        HardwareMessage message = new HardwareMessage();
-        message.content = new JSONObject(msg);
-        message.type = encode(message.content.getInt("type"));
-        return message;
+    public static HardwareMessage encodeMessage(String in){
+        HardwareMessage out = new HardwareMessage();
+        out.content = new JSONObject(in);
+        out.type = encode(out.content.getInt("type"));
+        return out;
+    }
+
+    public static String decodeMessage(HardwareMessage in){
+        JSONObject out = in.content;
+        out.put("type", decode(in.type));
+        return out.toString();
     }
 }
