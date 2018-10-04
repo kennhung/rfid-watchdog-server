805f1f338a05ccc82d3b305f07bed5fdafbf4e1f
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-10-04 21:19:51 +0800

    fix logger start before logger init

--- src/main/java/indi/kennhuang/rfidwatchdog/server/WatchdogServer.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/WatchdogServer.java
@@ -7,10 +7,11 @@ import indi.kennhuang.rfidwatchdog.server.util.logging.WatchDogLogger;
 
 public class WatchdogServer {
     private static DeviceServer deviceserver = new DeviceServer();
-    private static WatchDogLogger logger = new WatchDogLogger(LogType.Main);
+    private static WatchDogLogger logger;
 
     public static void main(String Args[]){
         WatchDogLogger.init();
+        logger = new WatchDogLogger(LogType.Main);
         new Thread(deviceserver).start();
         SQLite.openDatabase("jdbc:sqlite:foo.db");
         logger.info("Server initialized");
