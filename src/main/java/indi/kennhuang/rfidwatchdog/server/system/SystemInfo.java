19035b73531d3fd5dc76b68e3dbd302d5994d791
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-10-18 00:32:25 +0800

    add NetworkInterfaceInfo

---
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/system/SystemInfo.java
@@ -0,0 +1,39 @@
+package indi.kennhuang.rfidwatchdog.server.system;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.SocketException;
+import java.time.Duration;
+import java.time.Instant;
+
+public class SystemInfo {
+    public static final String systemVersion = "1.0 SNAPSHOT";
+    private static boolean started = false;
+
+    private static Instant startupTime;
+
+    public static void serverStart() {
+        if (!started) {
+            startupTime = Instant.now();
+        }
+    }
+
+    public static JSONObject getInfoInJson() throws SocketException {
+        JSONObject infoOut = new JSONObject();
+
+        infoOut.put("uptime", getSystemUptimeInSec());
+        infoOut.put("sysVersion", systemVersion);
+        infoOut.put("network", NetworkInterfaceInfo.getNetworkInterfaceInfo(true,false,false).toString());
+        return infoOut;
+    }
+
+
+    public static Duration getSystemUptime() {
+        return Duration.between(startupTime, Instant.now());
+    }
+
+    public static long getSystemUptimeInSec() {
+        return getSystemUptime().abs().getSeconds();
+    }
+}
