668024cd7174c4637459e8db5714794fdd8e2e0a
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-10-22 20:47:44 +0800

    change UsersHandler

--- src/main/java/indi/kennhuang/rfidwatchdog/server/web/wsHandler/UsersHandler.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/web/wsHandler/UsersHandler.java
@@ -3,7 +3,10 @@ package indi.kennhuang.rfidwatchdog.server.web.wsHandler;
 import indi.kennhuang.rfidwatchdog.server.module.User;
 import indi.kennhuang.rfidwatchdog.server.protocal.websocket.WebSocketHandler;
 import indi.kennhuang.rfidwatchdog.server.web.WebSocketServer;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
@@ -22,13 +25,29 @@ public class UsersHandler implements WebSocketHandler {
 
     public void getUsers(String data){
         try {
+            JSONArray usersOut = new JSONArray();
             List users = User.getAllUsers();
-            Iterator u = users.iterator();
-            while (u.hasNext()){
-                User user = (User) u.next();
-                System.out.println(user.name+" "+user.uid);
+            Iterator usersIterator = users.iterator();
+            while (usersIterator.hasNext()){
+                User user = (User) usersIterator.next();
+                JSONObject userOut = new JSONObject();
+                userOut.put("name",user.name);
+                userOut.put("uid",user.uid);
+                userOut.put("id",user.id);
+                userOut.put("metadata",user.metadata);
+                userOut.put("groups",user.groups);
+                userOut.put("door",user.getDoorsString());
+                usersOut.put(userOut);
             }
+            ws.send("usersList",usersOut.toString());
         } catch (SQLException e) {
+            try {
+                ws.sendInternalError(e.getMessage());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
