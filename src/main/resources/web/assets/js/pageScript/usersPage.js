322008719378e146b948f525fa9c2cb4f3d1b538
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-10-24 06:22:25 +0800

    add user list

--- src/main/resources/web/assets/js/pageScript/usersPage.js
+++ src/main/resources/web/assets/js/pageScript/usersPage.js
@@ -1,5 +1,30 @@
-websocket = new WatchdogWebsocket(6085, "/users", {
-    onload: function(event){
+var usersTable = $("#usersTable").DataTable({
+    "paging": false,
+    "ordering": true,
+    "info": true
+});
 
+$('#usersTable tbody').on('click', 'tr', function () {
+    var data = usersTable.row(this).data();
+    console.log(data);
+    $("#editId").val(data[0]);
+    $("#editUID").val(data[1]);
+    $("#editName").val(data[2]);
+    $("#editMeta").val(data[3]);
+    $("#editGroups").val(data[4]);
+    $("#editUserModal").modal('show');
+});
+
+
+websocket = new WatchdogWebsocket(6085, "/users", {
+    onload: function (event) {
+        websocket.send("getUsers", "all");
+    },
+    usersList: function (event) {
+        var userList = JSON.parse(event.data);
+        console.log(userList)
+        userList.forEach(function (data) {
+            usersTable.row.add([data.id, data.uid, data.name, data.metadata, data.groups]).draw();
+        })
     }
 });
