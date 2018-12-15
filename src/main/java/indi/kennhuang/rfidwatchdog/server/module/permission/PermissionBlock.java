ca64c7e25ceb61a76183a22df1ac28724ac770dd
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-15 16:48:37 +0800

    add encode and decode of PermissionBlock

--- src/main/java/indi/kennhuang/rfidwatchdog/server/module/permission/PermissionBlock.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/module/permission/PermissionBlock.java
@@ -1,5 +1,7 @@
 package indi.kennhuang.rfidwatchdog.server.module.permission;
 
+import org.json.JSONObject;
+
 public class PermissionBlock {
     public int targetId;
     public Permission permission;
@@ -15,5 +17,21 @@ public class PermissionBlock {
         this.permission = permission;
         this.validate = validate;
         targetId = target;
+    }
+
+    public static PermissionBlock encodePermissionBlock(JSONObject json){
+        PermissionBlock pb = new PermissionBlock();
+        pb.permission = Permission.encodePermission(json.getJSONObject("permission"));
+        pb.validate = json.getLong("validate");
+        pb.targetId = json.getInt("targetId");
+        return pb;
+    }
+
+    public static JSONObject decodePermissionBlock(PermissionBlock pb){
+        JSONObject out = new JSONObject();
+        out.put("targetId",pb.targetId);
+        out.put("validate",pb.validate);
+        out.put("permission", Permission.decodePermission(pb.permission));
+        return out;
     }
 }
