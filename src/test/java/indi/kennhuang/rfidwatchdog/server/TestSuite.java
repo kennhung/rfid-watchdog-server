104ec12d620a86bb1de15964c40ade98fc492691
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-15 17:27:03 +0800

    update test suites

--- src/test/java/indi/kennhuang/rfidwatchdog/server/TestSuite.java
+++ src/test/java/indi/kennhuang/rfidwatchdog/server/TestSuite.java
@@ -1,13 +1,14 @@
 package indi.kennhuang.rfidwatchdog.server;
 
 
+import indi.kennhuang.rfidwatchdog.server.module.ModuleTestSuite;
 import indi.kennhuang.rfidwatchdog.server.module.TestUser;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(value = Suite.class)
-@Suite.SuiteClasses(value={TestUser.class})
+@Suite.SuiteClasses(value={ModuleTestSuite.class})
 public class TestSuite {
 
 }
---
+++ src/test/java/indi/kennhuang/rfidwatchdog/server/module/ModuleTestSuite.java
@@ -0,0 +1,10 @@
+package indi.kennhuang.rfidwatchdog.server.module;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses(value={TestUser.class, TestDoor.class, TestGroup.class})
+public class ModuleTestSuite {
+
+}
