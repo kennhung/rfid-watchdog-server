d3d1a0e622a790bf683c68901715839ad4ad8066
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-25 14:35:18 +0800

    add insert

--- src/main/java/indi/kennhuang/rfidwatchdog/server/util/database/SQLGenerator.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/util/database/SQLGenerator.java
@@ -2,10 +2,20 @@ package indi.kennhuang.rfidwatchdog.server.util.database;
 
 public class SQLGenerator {
 
-    public static String getSelectString(String select, String from, String field, String target){
-        return "SELECT "+select+" FROM "+from+" where "+field+" is "+target;
+    public static String getSelectString(String select, String from, String field, String target) {
+        String out = "SELECT " + select + " FROM " + from;
+
+        if (field != null) {
+            out += " where " + field + " is " + target;
+        }
+
+        return out;
     }
 
-
+    public static String getInsertString(String target, String fields, String values) {
+        String out = "insert into " + target;
+        out += " (" + fields + " ) ( " + values + " )";
+        return out;
+    }
 
 }
