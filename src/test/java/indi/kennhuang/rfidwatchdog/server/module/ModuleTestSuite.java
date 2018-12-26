73805bbceb539fed794a798ccaa4b5ce3cbfd9aa
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-26 15:38:20 +0800

    add setting to test suite

--- src/test/java/indi/kennhuang/rfidwatchdog/server/module/ModuleTestSuite.java
+++ src/test/java/indi/kennhuang/rfidwatchdog/server/module/ModuleTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(value = Suite.class)
-@Suite.SuiteClasses(value={TestUser.class, TestDoor.class, TestGroup.class})
+@Suite.SuiteClasses(value={TestUser.class, TestDoor.class, TestGroup.class, TestSetting.class})
 public class ModuleTestSuite {
 
 }
