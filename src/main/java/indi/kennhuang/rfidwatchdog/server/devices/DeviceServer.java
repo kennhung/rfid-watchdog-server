36e4a8dfa6a7e97cba33ddaca34a4b9710492ef0
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-12-26 15:37:31 +0800

    fix device port can't be read by testcase

--- src/main/java/indi/kennhuang/rfidwatchdog/server/devices/DeviceServer.java
+++ src/main/java/indi/kennhuang/rfidwatchdog/server/devices/DeviceServer.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class DeviceServer  implements Runnable {
-    private static int serverPort;
+    static int serverPort;
 
     private static ServerSocket server = null;
     private static boolean shutdown = false;
